2daef5d59b9474b1ce645a584ea953c5e2c56c5b
吃饱了不饿 <ymzcode@163.com>
2022-11-22 11:27:05 +0800

    安装tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="flex">123123</h1>
   </main>
 </template>
