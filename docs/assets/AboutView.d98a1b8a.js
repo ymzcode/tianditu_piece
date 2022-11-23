97268097ae0b9ac831617cb564d72352f1bfce87
吃饱了不饿 <ymzcode@163.com>
2022-11-23 13:35:26 +0800

    部署更新

---
+++ docs/assets/AboutView.d98a1b8a.js
@@ -0,0 +1,1 @@
+import{o as _,c as n,a}from"./index.2a4cd6de.js";const r=(e,t)=>{const o=e.__vccOpts||e;for(const[s,c]of t)o[s]=c;return o},i={},u={class:"about"},l=a("h1",null,"This is an about page",-1),d=[l];function f(e,t){return _(),n("div",u,d)}const h=r(i,[["render",f]]);export{h as default};
