0034e284a561a8f7fca55ccb3485751fb0c0facb
吃饱了不饿 <ymzcode@163.com>
2022-11-25 18:47:55 +0800

    更改报错监听方式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,6 @@
 <script setup>
 import TianDiTu from "@/components/TianDiTu/TianDiTu.vue";
 import Setting from "@/components/TianDiTu/Setting.vue";
-
-createListener();
-
-/*
- * 注册监听报错的函数
- * */
-function createListener() {
-  window.addEventListener("error", (message) => {
-    // console.log(message)
-    window.$message.error(message.error.toString());
-  });
-}
 </script>
 
 <template>
