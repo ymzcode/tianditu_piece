91056f27c82e430c94c38b51ce8d40053f825798
吃饱了不饿 <ymzcode@163.com>
2022-11-27 10:23:43 +0800

    整理代码

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
 // 天地图的token
 const tianditu_token = "从控制台获取的token";
+// 版本号
+const version = '1.1.1'
 
-export { tianditu_token };
+export { tianditu_token, version };
--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.config.errorHandler = (err, instance, info) => {
-    // 处理错误，例如：报告给一个服务
-    // console.log(err.toString())
-    window.$message.error(err.toString());
-}
+  // 处理错误，例如：报告给一个服务
+  // console.log(err.toString())
+  window.$message.error(err.toString());
+};
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
