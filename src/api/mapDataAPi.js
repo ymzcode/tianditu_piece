eaee291b7de6e7379215bee4ed5a323af456f31c
吃饱了不饿 <ymzcode@163.com>
2022-11-28 10:59:22 +0800

    配置全国幼儿园数据接口

--- src/api/mapDataAPi.js
+++ src/api/mapDataAPi.js
@@ -1,8 +1,17 @@
 import { request } from "@/api/request";
 
+// 空气质量接口
 export function airQuality() {
   return request({
     url: "https://cd668ee7-8151-4ac6-aeeb-ab0fc9b91400.bspapp.com/air-quality",
-    method: "get",
+    method: "get"
   });
+}
+
+// 全国幼儿园数据接口
+export function nationalKindergarten() {
+  return request({
+    url: "https://cd668ee7-8151-4ac6-aeeb-ab0fc9b91400.bspapp.com/air-quality",
+    method: "get"
+  })
 }
--- src/stores/markerOptions.js
+++ src/stores/markerOptions.js
@@ -1,31 +1,36 @@
 import { defineStore } from "pinia";
-import kind from "@/assets/json/kindergarten.json";
+import kindergarten from "@/assets/json/kindergarten-new.json";
 import { useTiandituStore } from "@/stores/tianditu";
-import { airQuality } from "@/api/mapDataAPi";
+import { nationalKindergarten } from "@/api/mapDataAPi";
 
 export const useMarkerStore = defineStore("markerOptions", {
   state: () => ({}),
   actions: {
     /*
      * 初始化海量幼儿园数据*/
-    initKindergarten() {
-      console.log(kind);
-      airQuality().then((res) => {
-        console.log(res);
+    async initKindergarten(isUseLocalMapData = true) {
+      console.log(kindergarten);
+      let mapData;
+      // 判断是否使用本地数据
+      if (isUseLocalMapData) {
+        mapData = kindergarten.data;
+      } else {
+        const { data } = await nationalKindergarten();
+        mapData = data;
+      }
+
+      let lnglats = [];
+      mapData.results.map((item) => {
+        const co = item.geojson.geometry.coordinates[0];
+        const lng = new window.T.LngLat(co[0], co[1]);
+        lnglats.push(lng);
       });
-      // const { data } = kind;
-      // let lnglats = [];
-      // data.results.map((item) => {
-      //   const co = item.geojson.geometry.coordinates[0];
-      //   const lng = new window.T.LngLat(co[0], co[1]);
-      //   lnglats.push(lng);
-      // });
-      // const _CloudCollection = new window.T.CloudMarkerCollection(lnglats, {
-      //   color: "blue",
-      //   SizeType: window.TDT_POINT_SIZE_SMALL,
-      // });
-      // const { addOverLay } = useTiandituStore();
-      // addOverLay("kind", _CloudCollection);
+      const _CloudCollection = new window.T.CloudMarkerCollection(lnglats, {
+        color: "blue",
+        SizeType: window.TDT_POINT_SIZE_SMALL,
+      });
+      const { addOverLay } = useTiandituStore();
+      addOverLay("kind", _CloudCollection);
     },
   },
 });
