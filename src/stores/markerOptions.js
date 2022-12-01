be620727a53915fd746702a978e41b8b05ef8281
吃饱了不饿 <ymzcode@163.com>
2022-12-01 18:21:04 +0800

    优化加载风场

--- src/stores/markerOptions.js
+++ src/stores/markerOptions.js
@@ -103,38 +103,13 @@ export const useMarkerStore = defineStore("markerOptions", {
       clearInterval(this.kindergartenInterval);
     },
     initTmapWind() {
-      import("@/wind/index").then(({ WindLayer }) => {
+      import("tmap-wind").then(({ WindLayer }) => {
         fetch(
           "https://sakitam.oss-cn-beijing.aliyuncs.com/codepen/wind-layer/json/wind.json"
         )
           .then((res) => res.json())
           .then((res) => {
-            const pdefinedOverlay = new WindLayer(res, {
-              windOptions: {
-                velocityScale: 1 / 200,
-                frameRate: 60,
-                paths: 5000,
-                // eslint-disable-next-line no-unused-vars
-                colorScale: [
-                  "rgb(36,104, 180)",
-                  "rgb(60,157, 194)",
-                  "rgb(128,205,193 )",
-                  "rgb(151,218,168 )",
-                  "rgb(198,231,181)",
-                  "rgb(238,247,217)",
-                  "rgb(255,238,159)",
-                  "rgb(252,217,125)",
-                  "rgb(255,182,100)",
-                  "rgb(252,150,75)",
-                  "rgb(250,112,52)",
-                  "rgb(245,64,32)",
-                  "rgb(237,45,28)",
-                  "rgb(220,24,32)",
-                  "rgb(180,0,35)",
-                ],
-                lineWidth: 2,
-              },
-            });
+            const pdefinedOverlay = new WindLayer(res);
             console.log(pdefinedOverlay);
             const { addOverLay } = useTiandituStore();
             addOverLay("TmapWind", pdefinedOverlay);
