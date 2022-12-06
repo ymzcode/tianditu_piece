b29d040d3fc13dc1d6365bdcc7f96bf4ec8c1387
吃饱了不饿 <ymzcode@163.com>
2022-12-06 12:35:42 +0800

    添加聚合方法单独控制

--- src/stores/tianditu.js
+++ src/stores/tianditu.js
@@ -74,7 +74,10 @@ export const useTiandituStore = defineStore("tianditu", {
       delete this.mapControl[id];
     },
     /*
-     * 添加覆盖物*/
+     * 添加覆盖物
+     *
+     * 1. 如果是MarkerClusterer，请使用addMarkerClusterer
+     * */
     addOverLay(id, overlay) {
       if (!id || !overlay) {
         isShowErrorMessage && window.$message.error(`addOverLay 参数不完整`);
@@ -91,6 +94,8 @@ export const useTiandituStore = defineStore("tianditu", {
     },
     /*
      * 移除overLay覆盖物
+     *
+     * 1. 如果是MarkerClusterer，请使用removeMarkerClusterer
      * */
     removeOverLay(id) {
       if (!id) {
@@ -110,7 +115,7 @@ export const useTiandituStore = defineStore("tianditu", {
      * 添加某一种类的多个覆盖物
      * 该方法不会检查添加的视图是否重复存在，只会在相应的id中累计添加，
      * 如果没有该id，则根据固定命名规则创建ID后添加视图
-     * id ｜ 命名
+     * id ｜ 唯一命名
      * overlays ｜ 可以传入单个视图或者数组
      * 命名以_ARRAY_开头的覆盖物
      * 如 城市标点
@@ -121,6 +126,7 @@ export const useTiandituStore = defineStore("tianditu", {
           window.$message.error(`addOverLayForType 参数不完整`);
         throw new Error(`addOverLayForType 参数不完整`);
       }
+      // 固定规则命名，用于区分
       const ID = `_ARRAY_${id}`;
       let lays = [];
       // 判断overlays是不是一个数组
@@ -143,17 +149,77 @@ export const useTiandituStore = defineStore("tianditu", {
           window.$message.error(`removeOverLayForType 参数不完整`);
         throw new Error(`removeOverLayForType 参数不完整`);
       }
+      // 固定规则命名，用于区分
       const ID = `_ARRAY_${id}`;
       // 检查是否不存在
       if (!this.mapOverLay[ID]) {
         isShowErrorMessage &&
-        window.$message.error(`当前销毁的覆盖物类不存在:${ID}`);
+          window.$message.error(`当前销毁的覆盖物类不存在:${ID}`);
         throw new Error(`当前销毁的覆盖物类不存在:${ID}`);
       }
       this.mapOverLay[ID].map((item) => {
         this.Tmap.removeOverLay(item);
       });
       delete this.mapOverLay[ID];
+    },
+    /*
+    * 因为MarkerClusterer的特殊性，new 就会在地图中添加视图
+    * 单独写一个方法，用于在mapOverLay创建和删除
+    * 使用时无需执行new window.T.MarkerClusterer
+    * 只需要传入{
+          markers: markerArr,
+          ·······
+        }
+    * 方法内部执行完成后 返回视图
+    * 添加 MarkerClusterer
+    *
+    * id mapOverLay的唯一名称
+    * obj 创建视图的MarkerClustererOptions，
+    * */
+    addMarkerClusterer(id, MarkerClustererOptions) {
+      if (!id || !MarkerClustererOptions) {
+        isShowErrorMessage &&
+          window.$message.error(`addMarkerClusterer 参数不完整`);
+        throw new Error(`addMarkerClusterer 参数不完整`);
+      }
+      // 检查是否已经存在
+      if (this.mapOverLay[id]) {
+        isShowErrorMessage &&
+          window.$message.error(`当前存储的覆盖物已经存在:${id}`);
+        throw new Error(`当前存储的覆盖物已经存在:${id}`);
+      }
+      const _MarkerClusterer = new window.T.MarkerClusterer(this.Tmap, MarkerClustererOptions);
+      this.mapOverLay[id] = _MarkerClusterer;
+      /*
+       * this.Tmap.addOverLay(overlay);
+       * 没有执行该方法，因为本人尝试执行之后，会出现问题
+       * 问题现象：使用addOverLay和removeOverLay多次操作之后会出现无法聚合的问题
+       * 遂不执行
+       * */
+      return _MarkerClusterer;
+    },
+    /*
+     * 因为MarkerClusterer的特殊性，new 就会在地图中添加视图
+     * 单独写一个方法，用于在mapOverLay创建和删除
+     * 使用clearMarkers清除视图
+     *
+     * 删除 MarkerClusterer
+     * id 唯一命名
+     * */
+    removeMarkerClusterer(id) {
+      if (!id) {
+        isShowErrorMessage &&
+          window.$message.error(`removeMarkerClusterer 参数不完整`);
+        throw new Error(`removeMarkerClusterer 参数不完整`);
+      }
+      // 检查是否不存在
+      if (!this.mapOverLay[id]) {
+        isShowErrorMessage &&
+          window.$message.error(`当前销毁的覆盖物不存在:${id}`);
+        throw new Error(`当前销毁的覆盖物不存在:${id}`);
+      }
+      this.mapOverLay[id].clearMarkers();
+      delete this.mapOverLay[id];
     },
   },
 });
