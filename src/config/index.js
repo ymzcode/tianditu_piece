8e05408b41c435f2dfafa044ab071a70dd50f583
吃饱了不饿 <ymzcode@163.com>
2022-12-07 15:54:19 +0800

    将token转移到单独的文件中

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,4 @@
-// 天地图的token
-const tianditu_token = "从控制台获取的token";
-// tokens是为了防止demo资源被耗光，做了分发，正常业务使用上方即可
-const tianditu_tokens = [
-  "从控制台获取的token ",
-  "从控制台获取的token ",
-  "从控制台获取的token ",
-];
+import { tianditu_token, tianditu_tokens } from "@/config/tokens";
 // 版本号
 const version = "1.1.7";
 // 是否输出错误提示
