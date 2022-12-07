d6d23081d859023b1d6878f75c4237273aed2259
吃饱了不饿 <ymzcode@163.com>
2022-12-07 15:23:10 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,9 @@
 const tianditu_token = "从控制台获取的token";
 // tokens是为了防止demo资源被耗光，做了分发，正常业务使用上方即可
 const tianditu_tokens = [
-  "从控制台获取的token",
-  "从控制台获取的token",
-  "从控制台获取的token",
+  "从控制台获取的token ",
+  "从控制台获取的token ",
+  "从控制台获取的token ",
 ];
 // 版本号
 const version = "1.1.7";
