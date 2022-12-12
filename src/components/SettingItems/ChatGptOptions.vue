7661fdda45d903aafa7b1dedcfa02c2e44736c8e
吃饱了不饿 <ymzcode@163.com>
2022-12-12 22:39:54 +0800

    给聊天添加历史记录

--- src/components/SettingItems/ChatGptOptions.vue
+++ src/components/SettingItems/ChatGptOptions.vue
@@ -1,19 +1,44 @@
 <script setup>
-import { NList, NListItem, NInput, NButton } from "naive-ui";
+import { NList, NListItem, NInput, NButton, NEmpty } from "naive-ui";
 import { useChatGptOptionsStore } from "@/stores/chatGptOptions";
+import { computed } from "vue";
 
 const pinia_useChatGptOptionsStore = useChatGptOptionsStore();
+
+// 混排合并历史记录
+const historyChatArr = computed(() => {
+  const { userHistoryTextArr, chatGptHistoryTextArr } =
+    pinia_useChatGptOptionsStore;
+  const catchArr = [];
+  userHistoryTextArr.map((item, index) => {
+    catchArr.push(item);
+    chatGptHistoryTextArr[index] && catchArr.push(chatGptHistoryTextArr[index]);
+  });
+  return catchArr;
+});
 </script>
 
 <template>
   <n-list>
     <n-list-item>
+      <div class="mb-8">
+        <n-empty
+          v-if="pinia_useChatGptOptionsStore.userHistoryTextArr.length === 0"
+          description="还没有聊天记录"
+        >
+        </n-empty>
+        <div class="flex flex-col">
+          <div v-for="(item, index) in historyChatArr" :key="item.text + index">
+            {{ item.text }}
+          </div>
+        </div>
+      </div>
       <div class="flex flex-col">
         <n-input
           v-model:value="pinia_useChatGptOptionsStore.msgText"
           type="textarea"
           :autosize="{
-            minRows: 3,
+            minRows: 1,
           }"
           placeholder="试试看chatGpt能做什么"
         />
--- src/stores/chatGptOptions.js
+++ src/stores/chatGptOptions.js
@@ -6,22 +6,60 @@ export const useChatGptOptionsStore = defineStore("chatGptOptions", {
   state: () => ({
     // 文字输入内容
     msgText: "",
+    /*
+     * 机器人的历史回答
+     *
+     *
+     * */
+    chatGptHistoryTextArr: [],
+    /*
+     * 用户的历史输入
+     * */
+    userHistoryTextArr: [],
   }),
   actions: {
     sendChatGptMsg() {
+      this.saveUserHistoryText(this.msgText);
+      const textArr = [];
+      this.userHistoryTextArr.map((item, index) => {
+        textArr.push(item);
+        this.chatGptHistoryTextArr[index] &&
+          textArr.push(this.chatGptHistoryTextArr[index]);
+      });
+      let allMsg = "";
+      textArr.map((item) => {
+        allMsg += item.text;
+      });
       getChatGpt({
-        msg: this.msgText,
+        msg: allMsg,
       }).then((res) => {
         console.log(res);
         if (res.status === 200) {
           const { choices } = res.data;
-          choices.map((item) => {
-            this.msgText = this.msgText + item.text;
-          });
+          this.saveChatGptHistoryText(choices[0]);
+          if (choices[0].finish_reason === "length") {
+            // this.sendChatGptMsg();
+            console.log('接着请求')
+          }
         } else {
           window.$message.error("oops, 请求出现错误了！");
         }
       });
+    },
+    saveUserHistoryText(text) {
+      this.userHistoryTextArr.push({
+        time: Date.now(),
+        text: text,
+      });
+      this.msgText = "";
+    },
+    saveChatGptHistoryText(textObj) {
+      const length = this.chatGptHistoryTextArr.length;
+      if (textObj.finish_reason !== "stop") {
+        this.chatGptHistoryTextArr[length - 1].text += textObj.text;
+      } else {
+        this.chatGptHistoryTextArr.push(textObj);
+      }
     },
   },
 });
