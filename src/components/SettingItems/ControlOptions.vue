b1c02da282d56823b534c22286920d926e24cd17
吃饱了不饿 <ymzcode@163.com>
2022-12-22 09:58:39 +0800

    重排控件设置

--- src/components/SettingItems/ControlOptions.vue
+++ src/components/SettingItems/ControlOptions.vue
@@ -1,189 +1,147 @@
 <script setup>
-import { NSwitch, NDivider, NButton } from "naive-ui";
+import { NSwitch, NDivider, NButton, NList, NListItem } from "naive-ui";
 
 import { useControlOptionsStore } from "@/stores/controlOptions";
 const pinia_settingStore = useControlOptionsStore();
 </script>
 <template>
-  <!--        变量说明-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">当前已经将所有控件挂载</div>
-      <div class="text-xs text-gray-500">
-        你可以在控制台使用例如：window.$mapControl['scaleControl'].setColor('blue')
-        来改变比例尺的颜色。
+  <n-list>
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示左上角版权控件</span>
+        <n-switch
+          :value="pinia_settingStore.isShowCopyright"
+          @change="pinia_settingStore.switchShowCopyright"
+        />
       </div>
-    </div>
-    <div>window.$mapControl</div>
-  </div>
-  <n-divider />
-  <!--        是否显示左上角版权控件-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示左上角版权控件</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createCopyright"
+          >生成</n-button
+        >
+        <n-button type="warning" @click="pinia_settingStore.removeCopyright"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示缩放控件</span>
+        <n-switch
+          :value="pinia_settingStore.isShowZoomControl"
+          @change="pinia_settingStore.switchZoomControl"
+        />
       </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowCopyright"
-        @click="pinia_settingStore.switchShowCopyright()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createCopyright()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeCopyright()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
-  <n-divider />
-  <!--        是否显示缩放控件-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示缩放控件</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createZoomControl"
+          >生成</n-button
+        >
+        <n-button type="warning" @click="pinia_settingStore.removeZoomControl"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示比例尺</span>
+        <n-switch
+          :value="pinia_settingStore.isShowScaleControl"
+          @change="pinia_settingStore.switchScaleControl"
+        />
       </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowZoomControl"
-        @click="pinia_settingStore.switchZoomControl()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createZoomControl()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeZoomControl()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
-  <n-divider />
-  <!--        是否显示比例尺-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示比例尺</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createScaleControl"
+          >生成</n-button
+        >
+        <n-button type="warning" @click="pinia_settingStore.removeScaleControl"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示鹰眼图</span>
+        <n-switch
+          :value="pinia_settingStore.isShowOverviewMap"
+          @change="pinia_settingStore.switchOverviewMap"
+        />
       </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowScaleControl"
-        @click="pinia_settingStore.switchScaleControl()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createScaleControl()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeScaleControl()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
-  <n-divider />
-  <!--        是否显示鹰眼图-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示鹰眼图</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createOverviewMap"
+          >生成</n-button
+        >
+        <n-button type="warning" @click="pinia_settingStore.removeOverviewMap"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示地图类型</span>
+        <n-switch
+          :value="pinia_settingStore.isShowMapTypeControl"
+          @change="pinia_settingStore.switchMapTypeControl"
+        />
       </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowOverviewMap"
-        @click="pinia_settingStore.switchOverviewMap()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createOverviewMap()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeOverviewMap()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
-  <n-divider />
-  <!--        是否显示地图类型-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示地图类型</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createMapTypeControl"
+          >生成</n-button
+        >
+        <n-button
+          type="warning"
+          @click="pinia_settingStore.removeMapTypeControl"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+
+    <n-list-item>
+      <div class="flex flex-row">
+        <span class="mr-2">是否显示符号标绘</span>
+        <n-switch
+          :value="pinia_settingStore.isShowMilitarySymbols"
+          @change="pinia_settingStore.switchMilitarySymbols"
+        />
       </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowMapTypeControl"
-        @click="pinia_settingStore.switchMapTypeControl()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createMapTypeControl()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeMapTypeControl()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
-  <n-divider />
-  <!--        是否显示符号标绘-->
-  <div class="flex flex-row items-center justify-between">
-    <div class="flex flex-col setting-item-left">
-      <div class="item-title">是否显示符号标绘</div>
-      <div class="text-xs text-gray-500">
-        当前开关控制的是显隐，并不是销毁控件
-      </div>
-    </div>
-    <div>
-      <n-switch
-        :value="pinia_settingStore.isShowMilitarySymbols"
-        @click="pinia_settingStore.switchMilitarySymbols()"
-      />
-      <n-button
-        class="ml-2"
-        type="primary"
-        @click="pinia_settingStore.createMilitarySymbols()"
-        >生成</n-button
-      >
-      <n-button
-        class="ml-2"
-        type="warning"
-        @click="pinia_settingStore.removeMilitarySymbols()"
-        >销毁</n-button
-      >
-    </div>
-  </div>
+
+      <template #suffix>
+        <n-button
+          class="mb-2"
+          type="primary"
+          @click="pinia_settingStore.createMilitarySymbols"
+          >生成</n-button
+        >
+        <n-button
+          type="warning"
+          @click="pinia_settingStore.removeMilitarySymbols"
+          >销毁</n-button
+        >
+      </template>
+    </n-list-item>
+  </n-list>
 </template>
 
 <style scoped>
