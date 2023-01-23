8181c1107527036a51001372f1bcb256913fa6fe
吃饱了不饿 <ymzcode@163.com>
2023-01-23 11:41:11 +0800

    法环地图改用线上资源

--- src/stores/eldenRingoptions.js
+++ src/stores/eldenRingoptions.js
@@ -8,7 +8,8 @@ export const useEldenRingOptionsStore = defineStore("eldenRingOptions", {
      * 切换为游戏地图
      * */
     initEldenRingMap() {
-      const imageURL = "/img/elden-ring-map/IMG_MAP/{z}_{x}_{y}.jpg";
+      const imageURL =
+        "https://file.bingyishow.top/tianditu_piece/img/elden-ring-map/IMG_MAP/{z}_{x}_{y}.jpg";
       //创建自定义图层对象
       const lay = new window.T.TileLayer(imageURL, { minZoom: 1, maxZoom: 6 });
       const { Tmap, addTileLayer } = useTiandituStore();
